1950e96bb83171f5d42c63316ee308baddcc6f16
SogoHosomi <so5h0s0m1.1103@gmail.com>
2026-06-11 22:33:39 +0900

    322 - B

--- 06_10/pre_suf.py
+++ 06_10/pre_suf.py
@@ -2,23 +2,19 @@ n, m = map(int, input().split())
 s = input()
 t = input()
 
-#print(n, m, s, t)
 
-def func_pre(n, m ,s, t):
+def func_pre(n, s, t):
     for i in range(n):
-        if s[i] == t[i]:
-            if i == n - 1:
-                return 0
-    return 1
+        if s[i] != t[i]:
+            return 1
+    return 0
     
 def func_suf(n, m, s, t):
     for i in range(n):
-        if s[i] == t[m - n + i]:
-            if i == n - 1:
-                return 0
-    return 1
+        if s[i] != t[m - n + i]:
+            return 1
+    return 0
     
-pre = func_pre(n, m, s, t)
+pre = func_pre(n, s, t)
 suf = func_suf(n, m, s, t)
 print((pre << 1) | suf)
-
